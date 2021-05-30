fef89a50844e2b029befa22537ddceb7c43de5a8
Dakshraj Sharma <sharmarajdaksh@gmail.com>
2021-05-30 23:30:48 +0530

    Initialized spring-boot project

---
+++ src/test/java/sh/sharmarajdak/springpetclinic/SpringPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package sh.sharmarajdak.springpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
